9d640a08ac90cf05059036d2355347b24fe2f2e8
c-Nosetab <cpbateson@gmail.com>
2017-07-18 16:27:43 -0500

    fixed image upload reroute

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,14 +1,22 @@
 class ImagesController < ApplicationController
 
   def update
-    @employee = Employee.find(params[:id])
-    if @employee.update( employee_params )
-      flash[:success] = "Profile Image Changed"
-      redirect_to "/employees/#{@employee.id}"
+    if params[:employee]
+      @employee = Employee.find(params[:id])
+
+      if @employee.update( employee_params )
+        flash[:success] = "Profile Image Changed"
+        redirect_to "/employees/#{@employee.id}"
+      else
+        flash[:warning] = "Something went wrong, please try again"
+        redirect_to "/employees/#{@employee.id}"
+      end
+
     else
-      flash[:warning] = "Something went wrong, please try again"
-      redirect_to "/employees/#{@employee.id}"
+      flash[:warning] = "You Must Upload an Image Before Submitting"
+      redirect_to "/employees/#{current_user.id}"
     end
+
   end
 
 
